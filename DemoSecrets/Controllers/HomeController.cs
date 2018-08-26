ec67954df2e21dd6e1517f07d33d957e63cb51a9
Jayendran <Jayendran.arumugam@heal.com>
2018-08-26 16:55:25 +0530

    Add project files.

---
+++ DemoSecrets/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DemoSecrets.Models;
+
+namespace DemoSecrets.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public HomeController(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["myConnectionString"] = Configuration["myConnectionString"];
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
